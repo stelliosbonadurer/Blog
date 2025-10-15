7f0a4fedbfd526c05f5765d991226530f1d2d059
Stellios Bonadurer <stelliosbonadurer@gmail.com>
2025-10-15 01:34:39 -0600

    Initial Commit

---
+++ dajngo_project/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for dajngo_project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dajngo_project.settings")
+
+application = get_asgi_application()
---
+++ dajngo_project/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dajngo_project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dajngo_project.settings")
+
+application = get_wsgi_application()
